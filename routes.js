ba58afa710e5893e7418a41cee0f2994701cd68d
JohnMcDonald <20006166@mail.wit.ie>
2022-06-28 19:45:37 +0100

    template webstorm project

---
+++ routes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const dashboard = require("./controllers/dashboard.js");
+const about = require("./controllers/about.js");
+
+router.get("/", dashboard.index);
+router.get("/dashboard", dashboard.index);
+router.get("/about", about.index);
+router.post("/dashboard/addtodo", dashboard.addTodo);
+
+module.exports = router;
